5b17e47e5a1722b4f5ae30d55620d8ddcd35d109
smada2017 <saivardhanmada@gmail.com>
2018-02-21 00:34:09 -0500

    added database functionality

--- app/src/main/java/com/example/saimada/shelterfinder/Register.java
+++ app/src/main/java/com/example/saimada/shelterfinder/Register.java
@@ -1,25 +1,28 @@
 package com.example.saimada.shelterfinder;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
     EditText ed1,ed2, ed3;
     Button b1;
     private FirebaseAuth mAuth;
+    private DatabaseReference dbRef;
+
     Toolbar toolbar;
-    Spinner userType;
+    Switch userType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +38,20 @@ public class Register extends AppCompatActivity {
         ed1 = (EditText)findViewById(R.id.editText);
         ed2 = (EditText)findViewById(R.id.editText2);
         ed3 = (EditText)findViewById(R.id.editText3);
-        userType = (Spinner)findViewById(R.id.spinner2);
+        userType = (Switch)findViewById(R.id.switch1);
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+        dbRef = mFirebaseDatabase.getReference();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if( (ed3.getText().toString().compareTo(ed2.getText().toString()) == 0) && !(ed1.getText().toString() == "")) {
                     createAccount(ed1.getText().toString(),ed2.getText().toString());
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String userID = user.getUid();
+                    User userAdding = new User(ed1.getText().toString(), ed2.getText().toString(), userType.isChecked());
+                    dbRef.child("users").child(userID).setValue(user);
+
 
                 }else {
                     ed3.setError("Passwords Don't Match");
