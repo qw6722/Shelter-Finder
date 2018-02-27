a7448f283ce2fe76b8ca2fb55ed6ac82a04f4958
Rohit42 <rohit.mittapalli@gmail.com>
2018-02-26 22:46:30 -0500

    Finished m4

--- app/src/main/java/com/example/saimada/shelterfinder/Register.java
+++ app/src/main/java/com/example/saimada/shelterfinder/Register.java
@@ -1,6 +1,7 @@
 package com.example.saimada.shelterfinder;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,6 +12,9 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -55,30 +59,33 @@ public class Register extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                    FirebaseUser user;
                     if( (ed3.getText().toString().compareTo(ed2.getText().toString()) == 0) && !(ed1.getText().toString() == "")) {
-                    createAccount(ed1.getText().toString(),ed2.getText().toString());
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    if (user == null) {
-                        ed4.setError(user.toString());
-                    }
-                    String userID = user.getUid();
-                    if (userType.isChecked()) {
-                        if (ed4.getText().toString().equals(adminpassword)) {
-                            User userAdding = new User(ed1.getText().toString(), ed2.getText().toString(), true);
-                            dbRef.child("users").child(userID).setValue(userAdding);
+                    mAuth.createUserWithEmailAndPassword(ed1.getText().toString(),ed2.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser(); //You Firebase user
+                                String userID = user.getUid();
+                                if (userType.isChecked()) {
+                                    if (ed4.getText().toString().equals(adminpassword)) {
+                                        User userAdding = new User(ed1.getText().toString(), ed2.getText().toString(), true);
+                                        dbRef.child("users").child(userID).setValue(userAdding);
+                                    }
+                                    else {
+                                        ed4.setError("Admin Password Not Equal");
+                                    }
+                                }
+                                else {
+                                    User userAdding = new User(ed1.getText().toString(), ed2.getText().toString(), false);
+                                    dbRef.child("users").child(userID).setValue(userAdding);
+
+                                }
+                            }
                         }
-                        else {
-                            ed4.setError("Admin Password Not Equal");
-                        }
+                    });
                     }
                     else {
-                        User userAdding = new User(ed1.getText().toString(), ed2.getText().toString(), false);
-                        dbRef.child("users").child(userID).setValue(userAdding);
-
-                    }
-
-
-                }else {
                     ed3.setError("Passwords Don't Match");
                     //refresh editText fields
                 }
@@ -109,8 +116,6 @@ public class Register extends AppCompatActivity {
     }
 
     private void createAccount(String s, String s1) {
-        Log.e(s,mAuth.getCurrentUser().toString());
-        mAuth.createUserWithEmailAndPassword(s,s1);
         Log.e(s,s1);
     }
 
